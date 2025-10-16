046b0d5803fc13ddf99e13e003acf54715f07809
Ayoub-glitsh <ayoubaguezzar1@gmail.com>
2025-10-16 19:11:13 +0200

    Implement CRUD operations for stagiaires collection
    This script includes MongoDB operations for managing the 'stagiaires' collection, including deletion, insertion, updating, and querying of documents.

--- TP2_MongoDB_CRUD/TP2_MongoDB_CRUD_Solution.js
+++ TP2_MongoDB_CRUD/TP2_MongoDB_CRUD_Solution.js
@@ -1,1 +1,27 @@
 
+# MongoDB Exercise — Stagiaires Collection
+
+## Supprimer tous les documents
+db.stagiaires.deleteMany({})
+
+## Insérer plusieurs stagiaires
+db.stagiaires.insertMany([
+  { nom: "Ben", prenom: "Ali", age: 21, ville: "Ouarzazate", notes: [12,14] },
+  { nom: "El Amrani", prenom: "Sara", age: 22, ville: "Zagora", notes: [13,15] },
+  ...
+])
+
+## Mettre à jour certains champs
+db.stagiaires.updateOne({prenom:"Ali"}, {$set:{age:22}})
+db.stagiaires.updateOne({prenom:"Sara", nom:"El Amrani"}, {$push:{notes:18}})
+db.stagiaires.updateMany({ville:"Ouarzazate"}, {$push:{notes:20}})
+
+## Supprimer des documents
+db.stagiaires.deleteOne({nom:"El Fassi", prenom:"Nada"})
+db.stagiaires.deleteMany({ville:"Errachidia"})
+
+## Requêtes (Find)
+db.stagiaires.find({ville:"Zagora"},{ nom:1 , prenom:1 })
+db.stagiaires.find({ville:"Ouarzazate"},{ nom:1 , ville:1 })
+db.stagiaires.find({ville:"Tinghir"},{ prenom:1 , notes:1 })
+db.stagiaires.find({})
